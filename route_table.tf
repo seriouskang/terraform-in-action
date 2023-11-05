3c33a4565198c4a28c748b28d261e9fff1a42fef
seriouskang <scdean110@gmail.com>
2023-11-05 14:54:17 +0900

    feat(aws_route): igw 관련 aws_route 리소스 신규

--- route_table.tf
+++ route_table.tf
@@ -15,3 +15,10 @@ resource "aws_route_table_association" "route_table_association_2" {
     subnet_id      = aws_subnet.second_subnet.id
     route_table_id = aws_route_table.test_route_table.id 
 }
+
+# route to internet
+resource "aws_route" "test_route_igw" {
+    route_table_id = aws_route_table.test_route_table.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test_igw.id
+}
