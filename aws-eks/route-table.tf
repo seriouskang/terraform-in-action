6b9f975ea3b0b768ad8a9b6b2bcdcfab6e92e465
seriouskang <scdean110@gmail.com>
2023-11-06 21:22:47 +0900

    feat(route-table): private route table 신규

--- aws-eks/route-table.tf
+++ aws-eks/route-table.tf
@@ -4,3 +4,10 @@ resource "aws_route_table" "test_public" {
         Name = "${local.vpc_name}-public"
     }
 }
+
+resource "aws_route_table" "test_private" {
+    vpc_id = aws_vpc.this.id
+    tags = {
+      Name = "${local.vpc_name}-private"
+    }
+}
