91e845e3c6033afa2cffab2bebbd63b5967cf0b7
seriouskang <scdean110@gmail.com>
2023-11-06 21:25:53 +0900

    feat(route-table-association); private route table association 신규

--- aws-eks/route-table-association.tf
+++ aws-eks/route-table-association.tf
@@ -4,3 +4,10 @@ resource "aws_route_table_association" "test_public" {
     subnet_id      = aws_subnet.test_public[count.index].id
     route_table_id = aws_route_table.test_public.id
 }
+
+resource "aws_route_table_association" "test_private" {
+    count = length(local.private_subnets)
+
+    subnet_id      = aws_subnet.test_private[count.index].id
+    route_table_id = aws_route_table.test_private.id
+}
