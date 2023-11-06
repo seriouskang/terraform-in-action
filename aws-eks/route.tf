b35516757711d7f69d408fd8e7c20ac515da6dcd
seriouskang <scdean110@gmail.com>
2023-11-06 21:23:58 +0900

    feat(route): private route 신규

--- aws-eks/route.tf
+++ aws-eks/route.tf
@@ -3,3 +3,9 @@ resource "aws_route" "test_public_worldwide" {
     destination_cidr_block = "0.0.0.0/0"
     gateway_id             = aws_internet_gateway.this.id
 }
+
+resource "aws_route" "test_private_worldwide" {
+    route_table_id         = aws_route_table.test_private.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id         = aws_nat_gateway.this.id
+}
