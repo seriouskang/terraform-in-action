1b6cbdacfcd5c7c5afc7edcadf8ff344532c2775
seriouskang <scdean110@gmail.com>
2023-11-06 21:28:46 +0900

    feat(iam-role): eks cluster role 신규

---
+++ aws-eks/iam-role.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "test_cluster" {
+  name = "${local.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
--- aws-eks/locals.tf
+++ aws-eks/locals.tf
@@ -4,4 +4,5 @@ locals {
     public_subnets  = ["10.150.0.0/24", "10.150.1.0/24"]
     private_subnets = ["10.150.100.0/24", "10.150.101.0/24"]
     azs             = ["ap-southeast-1a", "ap-southeast-1c"]
+    cluster_name    = "test-eks"
 }
