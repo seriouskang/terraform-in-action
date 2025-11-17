96f3fee7d3986e757ede96eed6702303dbcd385d
seriouskang <scdean110@gmail.com>
2025-11-17 16:42:30 +0900

    feat: variable tutorial

--- local-basic/instance.tf
+++ local-basic/instance.tf
@@ -6,3 +6,9 @@ resource "aws_instance" "example" {
     ami           = "ami-0c1508b5372d244d7"
     instance_type = var.instance_type
 }
+
+resource "aws_instance" "map-example" {
+    ami           = "ami-0c1508b5372d244d7"
+    # instance_type = var.map_instance_type["example"]
+    instance_type = var.map_instance_type.example
+}
--- local-basic/variables.tf
+++ local-basic/variables.tf
@@ -2,3 +2,11 @@ variable "instance_type" {
     type    = string
     default = "t2.micro"
 }
+
+variable "map_instance_type" {
+    type    = map
+    default = {
+        "example" = "t2.micro"
+        "other_instance" = "t4g.micro"
+    }
+}
