c1d27bd081b08b74e4e28aa50980f897e4729c03
seriouskang <scdean110@gmail.com>
2025-11-17 18:01:38 +0900

    refactor(local-basic): instance.tf 정리

--- local-basic/instance.tf
+++ local-basic/instance.tf
@@ -4,11 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami           = "ami-0c1508b5372d244d7"
-    instance_type = var.instance_type
-}
-
-resource "aws_instance" "map-example" {
-    ami           = "ami-0c1508b5372d244d7"
+    # instance_type = var.instance_type
     # instance_type = var.map_instance_type["example"]
     instance_type = var.map_instance_type.example
 }
