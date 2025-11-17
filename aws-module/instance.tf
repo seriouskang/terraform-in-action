5c511d60076a724c10694ae308af0142a8e3f820
seriouskang <scdean110@gmail.com>
2025-11-17 20:21:23 +0900

    feat(aws-module): security group 추가

--- aws-module/instance.tf
+++ aws-module/instance.tf
@@ -3,8 +3,34 @@ resource "aws_instance" "example" {
     instance_type = var.instance_type
 
     subnet_id = module.vpc.public_subnets[0]
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
     tags = {
         Name = "example"
     }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["193.186.4.167/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
