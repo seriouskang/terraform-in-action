603eb8ce87a6c92e5c706771dffe2abb08dcbf35
seriouskang <scdean110@gmail.com>
2025-12-05 23:54:16 +0900

    feat: vpc

--- aws-basic/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "test_igw" {
-    vpc_id = aws_vpc.test_vpc.id
-
-    tags = {
-        Name = "test igw"
-    }
-}
--- aws-basic/vpc.tf
+++ aws-basic/vpc.tf
@@ -1,11 +1,86 @@
-provider "aws" {
-  region = "ap-northeast-2"
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+  
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "main"
+  }
 }
 
-resource "aws_vpc" "test_vpc" {
-  cidr_block = "10.150.0.0/16"
-  
+resource "aws_subnet" "main-public-1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.10.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-northeast-2a"
+
+    tags = {
+        Name = "main-public-1"
+    }
+}
+
+resource "aws_subnet" "main-public-2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.10.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-northeast-2c"
+
+    tags = {
+        Name = "main-public-2"
+    }
+}
+
+resource "aws_subnet" "main-private-1" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.10.101.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-northeast-2a"
+
   tags = {
-    Name = "test vpc"
+    Name = "main-private-1"
   }
+}
+
+resource "aws_subnet" "main-private-2" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.10.102.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "main-private-2"
+  }
+}
+
+resource "aws_internet_gateway" "main-gw" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags = {
+        Name = "main"
+    }
+}
+
+resource "aws_route_table" "main-public" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main-gw.id}"
+    }
+
+    tags = {
+        Name = "main-public-1"
+    }
+}
+
+resource "aws_route_table_association" "main-public-1" {
+    subnet_id = "${aws_subnet.main-public-1.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+
+resource "aws_route_table_association" "main-public-2" {
+    subnet_id = "${aws_subnet.main-public-2.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
 }
