a50d4cf45159a0941a696a9e31ef6ccd5fc55c23
seriouskang <scdean110@gmail.com>
2025-12-06 19:52:48 +0900

    feat: user-data 스크립트 개선

--- aws-basic/scripts/init.sh
+++ aws-basic/scripts/init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-sudo mkdir /data
+COUNTER=0
+
+while [ ! -b '${DEVICE}' ] && [ $COUNTER -lt 60 ]; do
+  echo "Waiting for ${DEVICE} to be available... ($COUNTER/60)"
+  sleep 1
+  COUNTER=$((COUNTER + 1))
+done
+
+if [ ! -b '${DEVICE}' ]; then
+  echo 'Error: Device ${DEVICE} did not become available within 60 seconds'
+  exit 1
+fi
+
+sudo mkdir -p /data
 echo '${DEVICE} /data ext4 defaults 0 0' | sudo tee -a /etc/fstab
 sudo mount -a
