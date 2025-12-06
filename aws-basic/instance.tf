19b5856d2ffb1c7c4e4b0849b014c6a0f8bff95d
seriouskang <scdean110@gmail.com>
2025-12-06 11:46:03 +0900

    feat: ebs 추가

--- aws-basic/instance.tf
+++ aws-basic/instance.tf
@@ -8,3 +8,19 @@ resource "aws_instance" "example" {
 
   key_name = aws_key_pair.mykeypair.key_name
 }
+
+resource "aws_ebs_volume" "ebs-volume-1" {
+  availability_zone = "ap-northeast-2a"
+  size = 20
+  type = "gp2"
+  
+  tags = {
+    Name = "extra volume data"
+  }
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+  device_name = "/dev/xvdh"
+  volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
+  instance_id = "${aws_instance.example.id}"
+}
