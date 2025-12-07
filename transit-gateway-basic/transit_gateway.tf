c9eafc267275b992d8f1cad0e5c40ca36e096240
seriouskang <scdean110@gmail.com>
2025-12-07 22:45:52 +0900

    feat(tgw-basic): tgw attachment 신규(vpc b, vpc c)

--- transit-gateway-basic/transit_gateway.tf
+++ transit-gateway-basic/transit_gateway.tf
@@ -20,3 +20,27 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_a" {
     Name = "tgw-lab-attachment-vpc-a"
   }
 }
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_b" {
+  subnet_ids         = [aws_subnet.vpc_b_subnet.id]
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id             = aws_vpc.vpc_b.id
+
+  dns_support = "enable"
+
+  tags = {
+    Name = "tgw-labl-attachment-vpc-b"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_c" {
+  subnet_ids         = [aws_subnet.vpc_c_subnet.id]
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id             = aws_vpc.vpc_c.id
+
+  dns_support = "enable"
+
+  tags = {
+    Name = "tgw-labl-attachment-vpc-c"
+  }
+}
