63946b936a5b5e3308f99c110fa7337c98bb634f
seriouskang <scdean110@gmail.com>
2025-12-07 22:02:23 +0900

    feat(tgw-basic): vpc-a igw, rtb 신규

--- transit-gateway-basic/vpc_a.tf
+++ transit-gateway-basic/vpc_a.tf
@@ -10,11 +10,33 @@ resource "aws_vpc" "vpc_a" {
 
 resource "aws_subnet" "vpc_a_subnet" {
   vpc_id                  = aws_vpc.vpc_a.id
-  cidr_block              = cidrsubnet(var.vpc_a_cidr, 8, 1)
+  cidr_block              = cidrsubnet(var.vpc_a_cidr, 8, 0)
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
     Name = "tgw-lab-vpc-a-public-subnet"
   }
+}
+
+resource "aws_internet_gateway" "vpc_a_igw" {
+  vpc_id = aws_vpc.vpc_a.id
+
+  tags = {
+    Name = "tgw-lab-vpc-a-igw"
+  }  
+}
+
+resource "aws_route_table" "vpc_a_public" {
+  vpc_id = aws_vpc.vpc_a.id
+
+  tags = {
+    Name = "tgw-labl-vpc-a-public-rt"
+  }
+}
+
+resource "aws_route" "vpc_a_internet" {
+  route_table_id         = aws_route_table.vpc_a_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc_a_igw.id
 }
