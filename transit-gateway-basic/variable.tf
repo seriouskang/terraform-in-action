b6d694336b7ca200cca853062c210080dfca845d
seriouskang <scdean110@gmail.com>
2025-12-07 22:14:41 +0900

    feat(tgw-basic): variable 추가

--- transit-gateway-basic/variable.tf
+++ transit-gateway-basic/variable.tf
@@ -5,16 +5,30 @@ variable "aws_region" {
 }
 
 variable "vpc_a_cidr" {
-    type = string
+    type    = string
     default = "10.100.0.0/16"
 }
 
 variable "vpc_b_cidr" {
-    type = string
+    type    = string
     default = "10.101.0.0/16"
 }
 
 variable "vpc_c_cidr" {
-    type = string
+    type    = string
     default = "10.102.0.0/16"
+}
+
+variable "my_ip" {
+    type = string
+}
+
+variable "instance_type" {
+    type    = string
+    default = "t3.micro"
+}
+
+variable "ami_id" {
+    type    = string
+    default = "ami-04fcc2023d6e37430"
 }
