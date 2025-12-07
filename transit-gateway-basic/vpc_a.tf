f40520af96a49ced22e9842b76f606a8494b4e53
seriouskang <scdean110@gmail.com>
2025-12-07 22:57:49 +0900

    feat(tgw-basic): sg 신규(vpc a)

--- transit-gateway-basic/vpc_a.tf
+++ transit-gateway-basic/vpc_a.tf
@@ -40,3 +40,47 @@ resource "aws_route" "vpc_a_internet" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.vpc_a_igw.id
 }
+
+resource "aws_security_group" "vpc_a" {
+  name_prefix = "tgw-lab-vpc-a-"
+  vpc_id      = aws_vpc.vpc_a.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      var.my_ip,
+      var.vpc_b_cidr,
+      var.vpc_c_cidr
+    ]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [
+      var.vpc_b_cidr,
+      var.vpc_c_cidr
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tgw-lab-vpc-a-sg"
+  }
+}
