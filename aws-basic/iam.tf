a0622e9c49f649efb7e62a843d20fc5ed06adcdb
seriouskang <scdean110@gmail.com>
2025-12-07 18:26:34 +0900

    refactor: iam policy attachment 구문 개선

--- aws-basic/iam.tf
+++ aws-basic/iam.tf
@@ -19,15 +19,8 @@ resource "aws_iam_group_membership" "admin-users" {
   group = "${aws_iam_group.admin.name}"
 }
 
-resource "aws_iam_policy_attachment" "admin-attach" {
-  name = "admin-attach"
-  groups = [
-    "${aws_iam_group.admin.name}"
-  ]
-  users = [
-    "admin",
-    "tf-user"
-  ]
+resource "aws_iam_group_policy_attachment" "admin-attach" {
+  group      = "${aws_iam_group.admin.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
